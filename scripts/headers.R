a6a6874f63a18a9b32b85eb54c271b9012bacdb9
Dave Kennedy <dsbkennedy@gmail.com>
2021-07-22 15:11:19 +0100

    Updated outline slide for RStudio Cloud

--- scripts/headers.R
+++ scripts/headers.R
@@ -4,3 +4,15 @@ knitr::purl("01-intro.Rmd", "scripts/01-intro.R")
 knitr::purl("02-management.Rmd", "scripts/02-management.R")
 knitr::purl("03-analysis.Rmd", "scripts/03-analysis.R")
 knitr::purl("04-visualisation.Rmd", "scripts/04-visualisation.R")
+
+knitr::knit_global("./00-outline.Rmd")
+
+
+rmd_files <- list.files(pattern=".Rmd", recursive=TRUE, full.names=TRUE)
+
+source_fn <- function(x) {
+  source(x, local = knitr::knit_global())
+}
+purrr::map(rmd_files,source_fn)
+
+purrr::map(source_fn,rmd_files)
