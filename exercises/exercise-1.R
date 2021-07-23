9b8e26915c1e6d49205f164d9c55e81c5eb83b6f
Dave Kennedy <dsbkennedy@gmail.com>
2021-07-23 11:44:04 +0100

    Fixed hyperlinks and added solutions folder

--- exercises/exercise-1.R
+++ exercises/exercise-1.R
@@ -1,14 +1,10 @@
 # Session 1
 
 # Load the readxl and here packages
-library(readxl)
-library(here)
 
 # Load the Excel file called africa_cdc_covid_data.xlsx and assign to an object called covid_data
-covid_data <- readxl::read_xlsx(here('data', 'africa_cdc_covid_data.xlsx'))
 
 #Describe the dataset. How many rows (observations) and columns (variables) does it have?
 
 # Save the object covid_data as a csv file in the data folder
-write.csv(covid_data, here('data', 'covid_data.csv'))
 
